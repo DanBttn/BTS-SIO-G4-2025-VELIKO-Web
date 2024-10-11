fbaad6a9bb324799fd209ad6216c866808060142
danbi <Dan.BITTON@ortmontreuil.fr>
2024-10-11 08:56:30 +0200

    page de suppression de compte

--- src/Controller/DeleteProfilController.php
+++ src/Controller/DeleteProfilController.php
@@ -2,32 +2,31 @@
 
 namespace App\Controller;
 
-use http\Env\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Config\Doctrine\Orm\EntityManagerConfig;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DeleteProfilController extends AbstractController
 {
     #[Route('/delete/profil', name: 'app_delete_profil')]
-    public function index(Request $request, EntityManagerConfig $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-
         $user = $this->getUser(); // Get the logged-in user
 
-        if (!$user) {
-            throw $this->createAccessDeniedException('Vous devez être connecté pour accéder à cette page.');
-        }
+
 
         // Vérifier le token CSRF
         if ($this->isCsrfTokenValid('delete_account', $request->request->get('_token'))) {
-            $entityManager->remove($user);
-            $entityManager->flush();
 
-            // Rediriger vers la déconnexion après suppression
+            // Anonymiser les données utilisateur
+            $this->anonymizeUser($user, $entityManager);
+
+            // Rediriger vers la déconnexion après anonymisation
             return $this->redirectToRoute('app_logout');
         }
+        //$this->redirectToRoute('app_register');
 
         // En cas de problème avec le token CSRF, rediriger avec un message d'erreur
         $this->addFlash('error', 'Problème lors de la suppression du compte.');
@@ -35,5 +34,22 @@ class DeleteProfilController extends AbstractController
         return $this->render('delete_profil/index.html.twig', [
             'controller_name' => 'DeleteProfilController',
         ]);
+    }
+
+    private  function anonymizeUser($user, EntityManagerInterface $entityManager): void
+    {
+        // Générer une série de 5 chiffres aléatoires
+        $randomNumber = random_int( 00000, 99999);
+
+        // Anonymiser les champs sensibles
+        $user->setEmail('anonymous_' . $randomNumber . '@veliko.local');
+        $user->setNom('anonymous_');
+        $user->setPrenom('anonymous_');
+        $user->setAdresse('');
+        $user->setPassword(md5(random_int(1,10000))); // Réinitialiser le mot de passe
+
+        // Sauvegarder les modifications
+        $entityManager->persist($user);
+        $entityManager->flush();
     }
 }
