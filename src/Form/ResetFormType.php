1f3af085beb95104ff941d31fe2999199a824a0e
Cscet <celia.scetbon0705@gmail.com>
2024-11-10 00:42:15 +0100

    ajout de contraintes pour reinitialiser son mot de passe son mot de passe

--- src/Form/ResetFormType.php
+++ src/Form/ResetFormType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class ResetFormType extends AbstractType
 {
@@ -20,11 +23,31 @@ $builder
 'placeholder' => 'Entrez votre nouveau mot de passe',
 ],
 'constraints' => [
-new Assert\NotBlank(['message' => 'Le mot de passe est requis']),
-new Assert\Length([
-'min' => 12,
-'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
-]),
+    new NotBlank([
+        'message' => 'Veuillez entrer un mot de passe',
+    ]),
+    new Length([
+        'min' => 12,
+        'minMessage' => 'Votre mot de passe doit contenir au moins  {{ limit }} caractères',
+        // max length allowed by Symfony for security reasons
+        'max' => 4096,
+    ]),
+    new Regex(
+        '/[a-z]/',
+        'Votre mot de passe doit contenir au moins une minuscule'
+    ),
+    new Regex(
+        '/[A-Z]/',
+        'Votre mot de passe doit contenir au moins une majuscule'),
+    new Regex(
+        '/[0-9]/',
+        'Votre mot de passe doit contenir au moins un chiffre'
+    ),
+    new Regex(
+        '/[^a-zA-Z0-9]/',
+        'Votre mot de passe doit contenir au moins un caractère spécial'
+    ),
+
 ],
 ])
 ->add('confirmPassword', PasswordType::class, [
