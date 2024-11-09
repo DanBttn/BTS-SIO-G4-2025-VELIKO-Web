92d592070740b43adaeca4f4919a6fe318f2fe0e
Cscet <celia.scetbon0705@gmail.com>
2024-11-10 00:40:31 +0100

    ajout de contraintes pour modifier son mot de passe

--- src/Form/PasswordChangeFormType.php
+++ src/Form/PasswordChangeFormType.php
@@ -7,6 +7,9 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class PasswordChangeFormType extends AbstractType
 {
@@ -24,11 +27,31 @@ class PasswordChangeFormType extends AbstractType
                 'label' => 'Nouveau mot de passe',
                 'required' => true,
                 'constraints' => [
-                    new Assert\NotBlank(),
-                    new Assert\Length([
-                        'min' => 12,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
                     ]),
+                    new Length([
+                        'min' => 12,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins  {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                    new Regex(
+                        '/[a-z]/',
+                        'Votre mot de passe doit contenir au moins une minuscule'
+                    ),
+                    new Regex(
+                        '/[A-Z]/',
+                        'Votre mot de passe doit contenir au moins une majuscule'),
+                    new Regex(
+                        '/[0-9]/',
+                        'Votre mot de passe doit contenir au moins un chiffre'
+                    ),
+                    new Regex(
+                        '/[^a-zA-Z0-9]/',
+                        'Votre mot de passe doit contenir au moins un caractère spécial'
+                    ),
+
                 ],
             ])
             ->add('confirm_password', PasswordType::class, [
